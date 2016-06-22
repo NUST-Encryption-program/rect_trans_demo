bb45c5083e710e561207a790d998853ec94f7321
haishuiaa <gaoyanshou1234@126.com>
2016-06-22 23:55:17 +0800

    init

---
+++ js/ngcontroller.js
@@ -0,0 +1,34 @@
+﻿var news =  new Array();
+var learning = new Array();
+var value = Math.floor(Math.random()*5) + 7
+var linevalue = Math.floor(Math.random()*5) + 7
+for(var i=0;i<value;i++)
+{
+	learning[i]={id:i}
+}
+
+for(var i=0;i<linevalue;i++)
+{
+	news[i]={id:i}
+}
+var div=document.getElementById('calculator');
+div.style.width=40*linevalue+'px';
+function NewsController($http,$scope) {
+		$http.get("json/question.json").success(function(data) {
+            $scope.newsItems = data.freetrial;
+		}).error(function(data) {  
+		    $scope.newsItems = news
+		});
+    }
+	
+
+function LearningController($http,$scope) {
+	    $http.get("json/learning.json").success(function(data) {
+            $scope.learningItems = data.freetrial;
+		}).error(function(data) {  
+		    $scope.learningItems = learning
+		});
+        $scope.remove = function (index) {
+            $scope.items.splice(index, 1);
+        }
+    }
